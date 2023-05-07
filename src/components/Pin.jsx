7bc2e92ffd1aa3dfcbfc2467948790599879034a
ShampaShaw <shampashaw088@gmail.com>
2023-05-08 00:00:44 +0530

    5th commit

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { v4} from 'uuid'
 import {v4 as uuidv4 } from 'uuid'
 import { MdDownloadForOffline } from 'react-icons/md'
 import { AiTwotoneDelete } from 'react-icons/ai'
@@ -44,7 +43,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save}}) => {
         client
          .delete(id)
          .then(() => {
-            window.location.reload()
+            window.location.reload();
          })
     }
 
@@ -57,15 +56,16 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save}}) => {
             className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out'
         >
          <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url()} />
-         {postHovered && (
+        {postHovered && (
             <div
                 className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
                 style={{height:'100%'}}
             >
                 <div className='flex items-center justify-between'>
                     <div className='flex gap-2'>
-                        <a
-                            href={`${image?.asset?.url}?dl=`}
+                        <a  
+                            href={`${image.asset.url}dl=`}
+                            //href={`${image?.asset?.url}?dl=`}
                             download
                             onClick={(e) => e.stopPropagation() }
                             className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none'
@@ -100,7 +100,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save}}) => {
                                 {destination.length > 20 ? destination.slice(8,20) : destination.slice(8)}
                             </a>
                         )}
-                        {postedBy?._id === user.googleId && (
+                        {postedBy?._id === user?.googleId && (
                             <button
                                 type='button'
                                 onClick={(e) => {
@@ -114,7 +114,8 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save}}) => {
                         )}
                 </div>
             </div>
-         )}
+
+                    )}
         </div>
         <Link to={`user-profile/${postedBy?._id}`} className='flex gap-2 mt-2 items-center'>
             <img
